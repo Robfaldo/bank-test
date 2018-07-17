34555aae707967229bfcee99d115dae9a9f61094
Robert Faldo <robertfaldo@gmail.com>
2018-07-17 10:38:42 +0100

    Add createTransactionLine in Account
    Before, there was DRY code that created a line to add to the
    transaction history, I extracted method to the
    createTransactionLine function

--- account.js
+++ account.js
@@ -15,41 +15,39 @@ class Account {
   }
 
   deposit (amount) {
-    this.throwErrorIfEmpty(amount, "Deposit")
+    this.throwErrorIfEmpty(amount, 'Deposit')
     this._balance += amount
-    var transactionLine = this._formatter.format(
-      {
-        date: dateFormat(new Date(), 'dd/mm/yyyy'),
-        deposit: amount,
-        withdraw: 0,
-        balance: this._balance
-      }
-    )
-    this._transactionsHistory.push(transactionLine)
+    var line = this.createTransactionLine({depositAmount: amount, withdrawAmount: 0})
+    this._transactionsHistory.push(line)
   }
 
   withdraw (amount) {
-    this.throwErrorIfEmpty(amount, "Withdraw")
+    this.throwErrorIfEmpty(amount, 'Withdraw')
     this._balance -= amount
-    var transactionLine = this._formatter.format(
-      {
-        date: dateFormat(new Date(), 'dd/mm/yyyy'),
-        deposit: 0,
-        withdraw: amount,
-        balance: this._balance
-      }
-    )
-    this._transactionsHistory.push(transactionLine)
+    var line = this.createTransactionLine({depositAmount: 0, withdrawAmount: amount})
+    this._transactionsHistory.push(line)
   }
 
   printStatement () {
     return this._printer.print(this._transactionsHistory)
   }
 
-  throwErrorIfEmpty(amount, transactionType) {
+  throwErrorIfEmpty (amount, transactionType) {
     if (amount === undefined) {
       throw new Error(`${transactionType} amount cannot be empty`)
     }
+  }
+
+  createTransactionLine (amount) {
+    var transactionLine = this._formatter.format(
+      {
+        date: dateFormat(new Date(), 'dd/mm/yyyy'),
+        deposit: amount.depositAmount,
+        withdraw: amount.withdrawAmount,
+        balance: this._balance
+      }
+    )
+    return transactionLine
   }
 }
 
